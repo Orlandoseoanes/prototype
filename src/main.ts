b3a667f84048706ca74176336a7f2e9d701b1320
orlandoseoanes <oaseoanes@unicesar.edu.co>
2025-10-20 15:20:08 -0500

    Habilitar CORS para permitir solicitudes desde el frontend en localhost:3000

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,10 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: "http://localhost:3000", // Frontend
+    credentials: true,
+  });
 
   const config = new DocumentBuilder()
     .setTitle('Patrón Prototype API')
